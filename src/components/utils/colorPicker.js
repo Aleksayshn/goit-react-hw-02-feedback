64ecf039893aea23c6af71fbc2ff83299bc67b4a
Oleksandra Tiankina <ag7756862@gmail.com>
2023-02-15 20:59:15 +0200

    refactor: delete comments

--- src/components/utils/colorPicker.js
+++ src/components/utils/colorPicker.js
@@ -1,13 +1,13 @@
 export const colorPicker = color => {
   switch (color) {
     case 'good':
-      return '#00b894'; // change 'good' color to a shade of green
+      return '#00b894';
     case 'neutral':
-      return '#ffeaa7'; // change 'neutral' color to a shade of yellow
+      return '#ffeaa7';
     case 'bad':
-      return '#d63031'; // change 'bad' color to a shade of red
+      return '#d63031';
     default:
-      return '#303030'; // use a dark gray as the default color
+      return '#303030';
   }
 };
 
